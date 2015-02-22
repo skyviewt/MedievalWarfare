6a3185751bf77ab6bb2a98e1cacda1ed2661889e
YifanZhou <yifan@Yifans-MacBook-Pro.local>
2015-02-22 13:05:42 -0500

    added village.cs and changed .gitignore
    Village.cs is mostly done. Commented out the codes that required
    unimplemented methods in other classes to avoid errors. Will remove the
    comments once they are implemented.
    
    Added “Library” to .gitignore

---
+++ Assets/Scripts/Village.cs
@@ -0,0 +1,231 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum VillageActionType
+{
+	ReadyForOrders,
+	BuildStageOne
+};
+
+public enum VillageType
+{
+	Hovel,
+	Town,
+	Fort
+}
+
+
+public class Village : MonoBehaviour {
+
+	private List<Tile> controlledRegion;
+	private Player controlledBy;
+	private Tile locatedAt;
+	private List<Unit> supportedUnits;
+	private VillageType myType;
+	private VillageActionType myAction;
+	private int gold;
+	private int wood;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//constructor
+	public Village(Player p, List<Tile> regions)
+	{
+		controlledRegion = regions;
+		controlledBy = p;
+		//LocatedAt needs to spawn a new Hovel
+		myType = VillageType.Hovel;
+		supportedUnits = new List<Unit>();
+		myAction = VillageActionType.ReadyForOrders;
+		gold = 0;
+		wood = 0;
+
+	}
+	
+	//setters and getters
+	public void setGold(int goldValue)
+	{
+		gold = goldValue;
+	}
+
+	public int getGold(){
+		return gold;
+	}
+	public void setWood(int woodValue)
+	{
+		wood = woodValue;
+	}
+	
+	public int getWood(){
+		return wood;
+	}
+
+	public VillageType getMyType()
+	{
+		return myType;
+	}
+
+	public ActionType getAction()
+	{
+		return myAction;
+	}
+
+	public Player getPlayer()
+	{
+		return controlledBy;
+	}
+
+	public List<Tile> getControlledRegion()
+	{
+		return controlledRegion;
+	}
+
+	public List<Unit> getControlledUnits()
+	{
+		return supportedUnits;
+	}
+
+	public void setLocation(Tile t)
+	{
+		locatedAt = t;
+	}
+
+	public Tile getLocatedAt()
+	{
+		return locatedAt;
+	}
+
+
+	//increment / decrement
+
+	public void addGold(int i)
+	{
+		gold += i;
+	}
+	public void removeGold(int i)
+	{
+		gold -= i;
+	}
+	public void addWood(int i)
+	{
+		wood += i;
+	}
+
+	public void removeWood(int i)
+	{
+		wood -= i;
+	}
+
+
+	public void addUnit(Unit u)
+	{
+		supportedUnits.Add (u);
+		u.setVillage (this);
+		//the following 2 lines may not be needed.
+		Tile t = u.getLocation ();
+		t.setOccupyingUnit (u);
+	}
+	//needs unit's setters and getters along with the Tombstone Landtype
+	/*
+	public void removeUnit(Unit u)
+	{
+		Tile unitLocation = u.getLocation ();
+		unitLocation.setOccupyingUnit (null);
+		u.setLocation (null);
+		u.setVillage(null);
+		unitLocation.setLandType(LandType.Tombstone);
+		supportedUnits.Remove(u);
+	}
+*/
+
+	//Needs setVillage in Tile. Remove comment once setVillage is implemented
+/*	public void addTile(Tile t)
+	{
+		controlledRegion.Add (t);
+		t.setVillage (this);
+	}
+*/
+	public void removeTile(Tile t)
+	{
+		controlledRegion.Remove (t);
+	}
+	
+	public void addRegion(List<Tile> regions)
+	{	
+		//doing exactly what the gameManager.addregion(List<Tile>, Village village) is doing
+		foreach (Tile t in regions) {
+			t.setVillage(this);
+			controlledRegion.Add(t);
+
+			//if there is a unit on the tile
+			Unit u = t.getOccupyingUnit();
+			if(u != null){
+				u.setVillage(this);
+				supportedUnits.Add(u);
+			}
+		}
+	}
+
+	//needs getWage in Units
+/*
+	public int getTotalWages()
+	{
+		int totalWage = 0;
+		foreach (Unit u in supportedUnits) {
+			int tempWage = u.getWage();
+			totalWage += tempWage;
+				}
+		return totalWage;
+	}
+*/
+
+	//needs setLocation and setVillage and setOccupyingUnit in Tile
+	/*
+	public void reireAllUnits()
+	{
+		foreach (Unit u in supportedUnits) {
+			Tile unitLocation = u.getLocation();
+			unitLocation.setOccupyingUnit(null);
+			unitLocation.setLandType(LandType.Tombstone);
+			u.setLocation(null);
+			u.setVillage(null);
+			supportedUnits.Remove(u);
+				}
+	}
+	*/
+
+
+	public void upgrade(VillageType newType)
+	{
+		//assuming gamemanager (or any manager) is doing all the heavy lifting in upgradeVillage
+		//only increments the villageType
+		myType += 1;
+
+	}
+	//sets gold to 0 and returns the previous gold value
+	public int pillageGold()
+	{
+		int previousGoldValue = gold;
+		gold = 0;
+		return previousGoldValue;
+	}
+	//sets wood to 0 and returns the previous wood value
+	public int pillagewood()
+	{
+		int previousWoodValue = wood;
+		wood = 0;
+		return previousWoodValue;
+	}
+
+}
