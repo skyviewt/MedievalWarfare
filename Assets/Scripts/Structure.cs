6a71a2610a82aca2412dc3bc7f02b9dc18103551
Jordan Quan <quan.jordan@gmail.com>
2015-02-23 00:02:38 -0500

    structure.cs
    got some comments, will be on trello or FB

---
+++ Assets/Scripts/Structure.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum UnitType
+{
+	Peasant,
+	Infantry,
+	Soldier,
+	Knight
+};
+
+public class Structure : MonoBehaviour {
+
+	private UnitType myType;
+	// private Tile locatedAt is the relation to its parent Tile
+	// this is not kept in script, but object hierarchy
+
+	// awake called on script instantiation
+	void Awake () {
+		myType = UnitType.Soldier; //assuming tower is of soldier level
+	}
+}
