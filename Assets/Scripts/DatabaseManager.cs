4b2f15a6e8a6546dbdd52b56ac8b0ebd0f21d792
Harvey Yang <harveyyang2002@gmail.com>
2015-04-02 20:17:47 -0400

    SQL branch

---
+++ Assets/Scripts/DatabaseManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Collections.Generic;
+
+public class DatabaseManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
