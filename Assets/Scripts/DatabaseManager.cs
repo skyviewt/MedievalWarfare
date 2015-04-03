7776cb7507528248e03ad5430ed2faa43e8c087a
Harvey Yang <harveyyang2002@gmail.com>
2015-04-02 21:21:53 -0400

    added .dll files for sql connections
    Added .dll files to ensure proper working stuff

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -3,11 +3,22 @@ using System.Collections;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 using System;
-using System.Data;
 using System.Collections.Generic;
 
 public class DatabaseManager : MonoBehaviour {
 
+	//MySQL instance specific items
+	string constr = "Server=localhost;Database=demo;User ID=demo;Password=demo;Pooling=true";
+	// connection object
+	MySqlConnection con = null;
+	// command object
+	MySqlCommand cmd = null;
+	// reader object
+	MySqlDataReader rdr = null;
+	// error object
+	MySqlError er = null;
+	// object collection array
+	
 	// Use this for initialization
 	void Start () {
 	
