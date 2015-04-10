1e9f04f5402cc32c3a90e9f612c9b2a85e5d4634
Telahnus <quan.jordan@gmail.com>
2015-04-09 21:34:23 -0400

    finished networking tower creation

--- Assets/Scripts/Structure.cs
+++ Assets/Scripts/Structure.cs
@@ -7,7 +7,17 @@ using System.Linq;
 public class Structure : MonoBehaviour {
 
 	private UnitType myType = UnitType.INFANTRY;
-	public GameObject structurePrefab;
 	public Village myVillage;
 	public Tile locatedAt;
+
+	[RPC]
+	public void initTower(NetworkViewID village, NetworkViewID tile){
+		transform.localScale = new Vector3 (0.03f, 0.03f, 0.03f);
+		transform.eulerAngles = new Vector3 (-90, 0, 0);
+		Village v = NetworkView.Find (village).GetComponent<Village>();
+		Tile t = NetworkView.Find (tile).GetComponent<Tile>();
+		myVillage = v;
+		locatedAt = t;
+	}
+
 }
